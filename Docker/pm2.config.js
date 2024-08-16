f1b4360321d7b104bb571c493616e06d52b8bb8f
HamzaYslmn <hamzayesilmen7@gmail.com>
2024-08-16 23:48:40 +0300

    Update

--- Docker/pm2.config.js
+++ Docker/pm2.config.js
@@ -2,19 +2,19 @@ module.exports = {
   apps: [
     {
       name: "gateway",
-      script: "./src/index.ts",  // Path to your gateway's entry file
+      script: "./out/index.js",
+      cwd: "./gateway",
       watch: false,
-      interpreter: "node",  // Ensure Node.js is used as the interpreter
       env: {
         NODE_ENV: "production",
         PORT: 8081
       }
     },
     {
-      name: "http-proxy",
-      script: "./proxy/index.js",  // Correct path to your proxy's entry file
+      name: "proxy",
+      script: "./index.js",
+      cwd: "./proxy",
       watch: false,
-      interpreter: "node",  // Ensure Node.js is used as the interpreter
       env: {
         NODE_ENV: "production",
         PORT: 8080
